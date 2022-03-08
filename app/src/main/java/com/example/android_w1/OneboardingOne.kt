35920a030838f8d8edc27492871e4eae6b361b44
QUÁCH VÕ ANH KHOA <18200147@student.hcmus.edu.vn>
2022-03-08 18:20:19 +0700

    done

---
+++ app/src/main/java/com/example/android_w1/OneboardingOne.kt
@@ -0,0 +1,11 @@
+package com.example.android_w1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OneboardingOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.onboarding_one)
+    }
+}
