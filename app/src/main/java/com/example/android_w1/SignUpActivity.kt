0baf3c7e1d59f43edcc1966111d90efc895d7ca7
LocNguyennn <nguyentanloc046@gmail.com>
2022-03-15 10:32:49 +0700

    update flow activity

--- app/src/main/java/com/example/android_w1/SignUpActivity.kt
+++ app/src/main/java/com/example/android_w1/SignUpActivity.kt
@@ -3,8 +3,6 @@ package com.example.android_w1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.widget.Button
 import androidx.appcompat.widget.AppCompatButton
 
 class SignUpActivity : AppCompatActivity() {
@@ -12,7 +10,7 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
 
-        val btnSignUp = findViewById<Button>(R.id.btnSignUp_SignUpPage)
+        val btnSignUp = findViewById<AppCompatButton>(R.id.btnSignUp_SignUpPage)
         val btnLogin = findViewById<AppCompatButton>(R.id.btnLogin_SignUpPage)
         val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
         btnLogin.setOnClickListener {
--- app/src/main/java/com/example/android_w1/WelcomeActivity.kt
+++ app/src/main/java/com/example/android_w1/WelcomeActivity.kt
@@ -4,10 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.widget.AppCompatButton
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome)
+        val btnSignUp = findViewById<AppCompatButton>(R.id.btnSignUp)
+        val btnLogin = findViewById<AppCompatButton>(R.id.btnLogin)
+
+        btnSignUp.setOnClickListener {
+            val intentSignUp = Intent(this@WelcomeActivity, SignUpActivity::class.java)
+            startActivity(intentSignUp)
+        }
+        btnLogin.setOnClickListener {
+            val intentLogin = Intent(this@WelcomeActivity, LoginActivity::class.java)
+            startActivity(intentLogin)
+        }
     }
 }
