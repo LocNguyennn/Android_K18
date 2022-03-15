ff19c39910a78463fba4cd627b21c4d6084d42dd
LocNguyennn <nguyentanloc046@gmail.com>
2022-03-15 16:48:56 +0700

    update sign up page

--- app/src/main/java/com/example/android_w1/SignUpActivity.kt
+++ app/src/main/java/com/example/android_w1/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_w1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 
 class SignUpActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.signup)
 
         val btnSignUp = findViewById<AppCompatButton>(R.id.btnSignUp_SignUpPage)
-        val btnLogin = findViewById<AppCompatButton>(R.id.btnLogin_SignUpPage)
+        val btnLogin = findViewById<TextView>(R.id.btnLogin)
         val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
         btnLogin.setOnClickListener {
             startActivity(intent)
