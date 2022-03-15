8dd83dfce9bb2e94a3bcd5380ecc1f6afb6ed50e
Khang Doan <khang.doan1311@gmail.com>
2022-03-15 13:13:43 +0700

    Update Login button activity

--- app/src/main/java/com/example/android_w1/User.kt
+++ app/src/main/java/com/example/android_w1/User.kt
@@ -1,4 +1,8 @@
 package com.example.android_w1
 
-class User {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class User(val email: String, val password: String) : Parcelable {
 }
