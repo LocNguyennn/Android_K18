213bc17cfe4a0aae3e85f5d8b5fca2be87eb3cd1
Khang Doan <khang.doan1311@gmail.com>
2022-03-15 13:06:04 +0700

    Update Login button activity

--- app/src/main/java/com/example/android_w1/LoginActivity.kt
+++ app/src/main/java/com/example/android_w1/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.android_w1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,27 @@ class LoginActivity : AppCompatActivity() {
 
         val btnLogin = findViewById<AppCompatButton>(R.id.btnLogin_LoginPage) // button login
         btnLogin.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.apply {
+                title = "Login"
+                setMessage("Incorrect email or password")
+                setNegativeButton("Return") { dialog, _ ->
+                    dialog.dismiss()
+                }
+            }
+            val email = findViewById<TextInputEditText>(R.id.emailInput).text.toString().trim()
+            val password = findViewById<TextInputEditText>(R.id.passwordInput).text.toString().trim()
+            if (email.equals("ronaldo@gmail.com") && password.equals("123456")) {
+                val user = User(email, password)
+                val intentLogin = Intent(this@LoginActivity, LoginActivity::class.java)
+                val bundle = Bundle()
+                bundle.putParcelable("user", user)
+                intentLogin.putExtras(bundle)
+                startActivity(intentLogin)
+            }
+            else {
+                builder.show()
+            }
 
         }
 
