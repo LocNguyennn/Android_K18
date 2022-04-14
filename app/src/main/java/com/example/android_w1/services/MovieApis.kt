fe8ddfef5bf838e73457a3d7d3ab148ab302c9a9
LocNguyennn <nguyentanloc046@gmail.com>
2022-04-14 10:45:30 +0700

    Create MovieApis.kt

---
+++ app/src/main/java/com/example/android_w1/services/MovieApis.kt
@@ -0,0 +1,21 @@
+package com.example.android_w1.services
+
+import com.example.android_w1.model.MovieResp
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApis {
+
+    @GET("movie/now_playing")
+    suspend fun listNowPlayMovies(
+        @Query("language") language: String,
+        @Query("page") page: Int,
+    ): MovieResp
+
+    //    ///movie/upcoming
+    @GET("movie/upcoming")
+    suspend fun listUpComingMovies(
+        @Query("language") language: String,
+        @Query("page") page: Int,
+    ): MovieResp
+}
