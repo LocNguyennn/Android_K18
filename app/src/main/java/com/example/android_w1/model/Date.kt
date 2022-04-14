54447a8145f95ad6cbb23e52cad376bd163e5b57
LocNguyennn <nguyentanloc046@gmail.com>
2022-04-14 10:42:05 +0700

    commit models
    Dates, Movie, MovieResp, OrginalLanguage

---
+++ app/src/main/java/com/example/android_w1/model/Date.kt
@@ -0,0 +1,6 @@
+package com.example.android_w1.model
+
+data class Dates (
+    val maximum: String? = null,
+    val minimum: String? = null
+)
---
+++ app/src/main/java/com/example/android_w1/model/OriginalLanguage.kt
@@ -0,0 +1,8 @@
+package com.example.android_w1.model
+
+enum class OriginalLanguage {
+    En,
+    Es,
+    Fr,
+    Ja
+}
